a58b7f6ce6917d025ee5b1d459377168966bde38
stefanolomo <stefanolomo@gmail.com>
2022-09-18 18:13:16 -0300

    Honestly I dont know how the hecc I fixed that bug

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
-let seccion = document.querySelector(".list")
+seccion = document.querySelector(".list")
 function create(){
+	//! Crea Los elementos
 	let newTask = document.createElement("div");
 	let section1 = document.createElement("section");
 	let section2 = document.createElement("section");
@@ -7,6 +8,18 @@ function create(){
 	let h5 = document.createElement("h5");
 	let button1 = document.createElement("button");
 	let button2 = document.createElement("button");
+	//! Pone las clases por defecto
+	p.setAttribute('class', "item");
+	h5.setAttribute('class', "item");
+	button1.setAttribute('class', "item");
+	button2.setAttribute('class', "item");
+	//? Verifica si es gasto o ingreso y acomoda las clases en cada caso
+	if (true){
+	h5.setAttribute('class', "ingreso");
+	}if (true){
+		h5.setAttribute('class', "gasto");
+	}
+
 	newTask.append(section1)
 	newTask.append(section2)
 	section1.append(p)
