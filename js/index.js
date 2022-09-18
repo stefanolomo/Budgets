462bb4093507e4cfe79b0a2a70b938d2dba795e8
stefanolomo <stefanolomo@gmail.com>
2022-09-18 16:38:05 -0300

    Ahora cuando tocas el boton se crea un item

--- js/index.js
+++ js/index.js
@@ -3,7 +3,15 @@ function create(){
 	let newTask = document.createElement("div");
 	let section1 = document.createElement("section");
 	let section2 = document.createElement("section");
+	let p = document.createElement("p");
+	let h5 = document.createElement("h5");
+	let button1 = document.createElement("button");
+	let button2 = document.createElement("button");
 	newTask.append(section1)
 	newTask.append(section2)
+	section1.append(p)
+	section2.append(h5)
+	section2.append(button1)
+	section2.append(button2)
 	seccion.append(newTask)
 };
