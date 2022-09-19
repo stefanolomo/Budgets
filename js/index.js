7a07d183228e357c7b56a33c57168a04411844a7
stefanolomo <stefanolomo@gmail.com>
2022-09-18 21:08:52 -0300

    pre finish

--- js/index.js
+++ js/index.js
@@ -12,9 +12,18 @@ function create(){
 	let p = document.createElement("p");
 	let h5 = document.createElement("h5");
 	let button = document.createElement("button");
+	let sp1 = document.createElement("span");
+	let sp2 = document.createElement("span");
+	let sp3 = document.createElement("span");
 	//! Pone las clases por defecto y la X y el boton de editar
 	p.setAttribute('class', "item");
+	sp1.innerText = nombre.value
+	sp2.innerText = " Amount: " + cantidad.value + " "
+	sp3.innerText = "Date: " + fecha.value + " "
 	button.setAttribute('class', "item red");
+	sp1.setAttribute("class", "name")
+	sp2.setAttribute("class", "quant")
+	sp3.setAttribute("class", "dat")
 	button.innerText = "X"
 	//? Verifica si es gasto o ingreso y acomoda las clases en cada caso
 	if (select.options[select.selectedIndex].value == "ingreso"){
@@ -25,8 +34,16 @@ function create(){
 		h5.setAttribute('class', "item gasto");
 		h5.innerText = "Spent " + precio.value + "$"
 	};
+	if (select.options[select.selectedIndex].value == "Type"){
+		h5.setAttribute('class', "item ingreso");
+		h5.innerText = "Earned " + precio.value + "$"
+	};
+	//? Une todo con pegamento y lo envia al HTML
 	newTask.append(section1)
 	newTask.append(section2)
+	p.append(sp1)
+	p.append(sp2)
+	p.append(sp3)
 	section1.append(p)
 	section2.append(h5)
 	section2.append(button)
