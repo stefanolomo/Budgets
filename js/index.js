5d9b1a08763bc761163eac668979a78ff25a9ec1
stefanolomo <stefanolomo@gmail.com>
2023-02-06 13:19:17 -0300

    JS create card completed and deletecard funtion done

--- js/index.js
+++ js/index.js
@@ -1,51 +1,62 @@
-let seccion = document.querySelector(".list");
-let select = document.querySelector(".tipo");
-let nombre = document.querySelector("#nombre");
-let cantidad = document.querySelector("#cantidad");
-let fecha = document.querySelector("#fecha");
-let precio = document.querySelector("#precio");
-function create(){
-	//! Crea Los elementos
-	let newTask = document.createElement("div");
-	let section1 = document.createElement("section");
-	let section2 = document.createElement("section");
-	let p = document.createElement("p");
-	let h5 = document.createElement("h5");
-	let button = document.createElement("button");
-	let sp1 = document.createElement("span");
-	let sp2 = document.createElement("span");
-	let sp3 = document.createElement("span");
-	//! Pone las clases por defecto y la X y el boton de editar
-	p.setAttribute('class', "item");
-	sp1.innerText = nombre.value
-	sp2.innerText = " Amount: " + cantidad.value + " "
-	sp3.innerText = "Date: " + fecha.value + " "
-	button.setAttribute('class', "item red");
-	sp1.setAttribute("class", "name")
-	sp2.setAttribute("class", "quant")
-	sp3.setAttribute("class", "dat")
-	button.innerText = "X"
-	//? Verifica si es gasto o ingreso y acomoda las clases en cada caso
-	if (select.options[select.selectedIndex].value == "ingreso"){
-		h5.setAttribute('class', "item ingreso");
-		h5.innerText = "Earned " + precio.value + "$"
-	};
-	if (select.options[select.selectedIndex].value == "gasto"){
-		h5.setAttribute('class', "item gasto");
-		h5.innerText = "Spent " + precio.value + "$"
-	};
-	if (select.options[select.selectedIndex].value == "Type"){
-		h5.setAttribute('class', "item ingreso");
-		h5.innerText = "Earned " + precio.value + "$"
-	};
-	//? Une todo con pegamento y lo envia al HTML
-	newTask.append(section1)
-	newTask.append(section2)
-	p.append(sp1)
-	p.append(sp2)
-	p.append(sp3)
-	section1.append(p)
-	section2.append(h5)
-	section2.append(button)
-	seccion.append(newTask)
+//° |------------------------|
+//° |		Index.js		 |
+//° |------------------------|
+
+//* Variables
+
+let FormTitle = document.querySelector("main section.menu form input#title");
+let FormQuant = document.querySelector("main section.menu form input#quant");
+let FormPrice = document.querySelector("main section.menu form input#price");
+let FormDate = document.querySelector("main section.menu form input#date"); // FormDate.valueAsDate.toLocaleDateString() '21/12/121'
+let FormButton = document.querySelector(
+	'main section.menu form button[type="submit"]'
+);
+let CardContainer = document.querySelector("main section.container");
+let RemoveCardButton = document.querySelector(
+	"main section.container .card .buttons button"
+);
+
+//* CreateCard Function
+let CreateCard = () => {
+	const template = `<div class="card">
+	<div class="details">
+		<b class="title">${EscapeHTML(FormTitle.value)}</b>
+		<p class="quant"><b>Quantity:</b> ${EscapeHTML(FormQuant.value)}</p>
+		<p class="price"><b>Price:</b> <code>$${EscapeHTML(FormPrice.value)}</code></p>
+	</div>
+	<div class="buttons">
+	<button onclick="DeleteCard(this.parentElement.parentElement)">
+	<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+		 class="bi bi-x-lg" viewBox="0 0 16 16">
+		<path
+			  d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
+	</svg>
+</button>
+	</div>
+</div>`;
+	if (
+		(FormTitle.value !== "") &
+		(FormPrice.value !== "") &
+		(FormQuant.value !== "")
+	) {
+		console.log(FormTitle, FormPrice, FormQuant);
+		CardContainer.insertAdjacentHTML("beforeend", template);
+	} else {
+		alert("The fields of title, price or quantity cant be empty!");
+	}
 };
+let DeleteCard = (element) => {
+	console.log(element);
+	element.remove();
+};
+
+function EscapeHTML(str) {
+	return str
+		.replace(/&/g, "&amp;")
+		.replace(/</g, "&lt;")
+		.replace(/>/g, "&gt;")
+		.replace(/"/g, "&quot;")
+		.replace(/'/g, "&#039;");
+}
+//* CreateCard Event listener on submit button
+FormButton.addEventListener("click", CreateCard);
